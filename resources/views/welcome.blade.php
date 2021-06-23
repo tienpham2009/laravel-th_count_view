b44edb114ad1debae7c7c8b96510bfda487e4eaf
tienpham2009 <tiendx2009@gmail.com>
2021-06-23 21:47:27 +0700

    count view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.master')
+@section('content')
+    <div class="title m-b-md">
+        Sử dụng Session đếm số lượng lượt xem
+    </div>
+
+    <div class="links">
+        <a href="{{ route('index') }}">Danh sách sản phẩm</a>
+    </div>
+@endsection
